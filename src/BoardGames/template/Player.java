f5417e0daecf88e0b3aee8443c6ee34977027081
thekingofpaidaxing <945737596@qq.com>
2023-03-30 21:31:07 +0800

    redo

---
+++ src/BoardGames/template/Player.java
@@ -0,0 +1,14 @@
+package BoardGames.template;
+
+import BoardGames.goBang.GoBangChessPieces;
+import BoardGames.goBang.GoBangRules;
+
+public abstract class Player {
+    public int chessType;
+    public int x_index;
+    public int y_index;
+
+    public void play(GoBangChessPieces pieces, GoBangRules gameStatus) {
+
+    }
+}
